0fcfcb6c5f1e1f63e7d29a2edb5f304474be5775
toneymar <toneymar@grinnell.edu>
2020-05-21 16:24:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.inventorymanagement
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        goToAddProduct.setOnClickListener {
+            startActivity(Intent(this, AddProductActivity::class.java))
+        }
+    }
+}
