067066b8f64c955eb26badd1954fafdbb8b42292
toneymar <toneymar@grinnell.edu>
2020-05-21 16:26:24 -0700

    test

--- app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -8,5 +8,6 @@ class AddProductActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
+
     }
 }
